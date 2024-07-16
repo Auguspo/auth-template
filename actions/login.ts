6729bc1c2b7c0e36f8d1b3c37e6544bf12fa5478
Auguspo <augustpolet@gmail.com>
2024-07-16 16:12:02 -0300

    Reset Password form

--- actions/login.ts
+++ actions/login.ts
@@ -8,7 +8,7 @@ import { getUserByEmail } from "@/data/user";
 import { sendVerificationEmail } from "@/lib/mail";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { generateVerificationToken } from "@/lib/token";
-import { send } from "process";
+
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values);
--- schemas/index.ts
+++ schemas/index.ts
@@ -1,16 +1,20 @@
 import * as z from "zod";
 
-export const ResetSchema = z.object({
-  email: z.string().email({message: "Email is required"}),
+export const NewPasswordSchema = z.object({
+  password: z.string().min(6,{ message: "Minium of 6 characters required" }),
 });
+
+export const ResetSchema = z.object({
+  email: z.string().email({ message: "Email is required" }),
+});
+
 export const LoginSchema = z.object({
-  email: z.string().email({message: "Email is required"}),
+  email: z.string().email({ message: "Email is required" }),
   password: z.string().min(1, { message: "Password is required" }),
 });
 
-
 export const RegisterSchema = z.object({
-  email: z.string().email({message: "Email is required"}),
+  email: z.string().email({ message: "Email is required" }),
   password: z.string().min(6, { message: "Minimun six caracters is required" }),
   name: z.string().min(1, { message: "Name is required" }),
 });
